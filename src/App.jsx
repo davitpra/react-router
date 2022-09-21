65c07aaacf52f7ebe9c5a543d66ea44e6d20874f
David <j.david.prado1716@gmail.com>
2022-09-21 18:25:37 -0500

    componentes de navegación

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Menu } from './Menu';
+import { HomePage } from './HomePage';
+import { BlogPage } from './BlogPage';
+import { ProfilePage } from './ProfilePage';
+
+// /#/ -> Home
+// /#/blog
+// /#/profile
+// /#/lalalala -> Not Found
+// /blog, /lalala -> Home
+
+function App() {
+  return (
+    <>
+      <HashRouter>
+        <Menu />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<p>Not found</p>} />
+        </Routes>
+      </HashRouter>
+    </>
+  );
+}
+
+export default App;
